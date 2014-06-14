ed899c444d1a8622580f6d551a4664e1577c4cf4
luis mori <luismorith@gmail.com>
2014-06-14 02:04:56 -0500

    usando fetch  y ordenando todo el mvc

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
 	console.log('main.js loaded');
+
+	window.collections.articles = new Puls3.Collections.Articles();
+
+	// hace un ajax a /articles con get
+	// todo lo obtenido lo pasa a json y a modelos dentro de la collecion
+	window.collections.articles.fetch(); 
+
 });
