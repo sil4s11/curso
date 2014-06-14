672858df54cc38279c2fa2404a97b1a9a7f5937d
luis mori <luismorith@gmail.com>
2014-06-14 04:01:32 -0500

    agregando la funcionalidad de votos

--- public/js/backbone/views/article-view.js
+++ public/js/backbone/views/article-view.js
@@ -1,10 +1,17 @@
 Puls3.Views.Article = Backbone.View.extend({
+	events:{
+		"click .votos .up":"upvote",
+		"click .votos .down":"downvote"
+	},
 	 tagName:'article',
 	 className:'post',
 	 initialize: function (){
-
+	 	var self = this
 	 	 this.template = _.template( $("#article-template").html());
 	 	//this.template = swig.compile( $("#article-template").html() );
+	 	this.model.on("change",function(){
+	 		self.render();
+	 	});
 
 	 },
 	 render : function (){
@@ -13,5 +20,19 @@ Puls3.Views.Article = Backbone.View.extend({
 	 	var html = this.template(data); 
 	 	//ACTUALIZA EL HTML DEL VIEW
 	 	this.$el.html( html );
+	 },
+	 upvote:function(e){
+	 	e.preventDefault();
+	 	var votes = parseInt( this.model.get('votes') , 10 );
+	 	this.model.set('votes', ++votes);
+	 	this.model.save();
+
+	 },
+	 downvote:function(){
+	 	var votes = parseInt( this.model.get('votes') , 10 );
+	 	this.model.set('votes', --votes);
+	 	this.model.save();
+
+
 	 }
 });
